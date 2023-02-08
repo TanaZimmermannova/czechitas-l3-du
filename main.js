7e2c5d059f74e0a6104151069cfcc16851dc6693
Táňa Zimmermannová <tana.zimmermannova@seznam.cz>
2023-02-08 20:26:29 +0100

    ukol 3

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // tady je místo pro náš program
 
+function stisk() {
+    let jmeno = prompt("Zadejte své jméno");
+    let prijmeni = prompt("Zadejte své příjmení");
+    let vek = Number(prompt("Zadejte svůj věk"));
+    let barva = prompt("Zadejte svoji oblíbenou barvu (v angličtině)");
+
+    let vystup = document.querySelector (".vystupProgramu");
+    vystup.innerHTML = jmeno + " " + prijmeni + ", " + vek;
+    vystup.style.color = barva;
+}
+
 
 
 
